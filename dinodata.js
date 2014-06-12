b7c76d025dccf8d3510d714c052442eda4d020ea
dev/iowa <deviowa@Thomass-MacBook-Air-2.local>
2014-06-12 13:47:29 -0500

    added dino functions

---
+++ dinodata.js
@@ -0,0 +1,28 @@
+//Dinosaur Data for To-do list Dev/Iowa project
+
+
+function create (title) {
+	// body...
+}
+
+function getAll () {
+	// body...
+}
+
+function get (id) {
+	// body...
+}
+
+function delete (id) {
+	// body...
+}
+
+function setStatus (id, status) {
+	// body...
+}
+
+
+function setTitle (id, title) {
+	// body...
+}
+
