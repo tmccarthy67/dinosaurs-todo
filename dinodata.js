cfed94ea1ad1fd56ea634c669eb59f940bce03d8
dev/iowa <deviowa@Thomass-MacBook-Air-2.local>
2014-06-12 14:02:53 -0500

    added dino more function

--- dinodata.js
+++ dinodata.js
@@ -1,19 +1,33 @@
 //Dinosaur Data for To-do list Dev/Iowa project
 
+var allToDos = new Array();
+
+function createGuid() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+	return v.toString(16);
+	});
+}
 
 function create (title) {
-	// body...
+	var newItem = {
+		"title":title,
+		"completed":false,
+		"id": createGuid()
+	};
+	allToDos.push(newItem);
+	return newItem.id;
 }
 
 function getAll () {
-	// body...
+	return allToDos;
 }
 
 function get (id) {
 	// body...
 }
 
-function delete (id) {
+function remove (id) {
 	// body...
 }
 
@@ -26,3 +40,8 @@ function setTitle (id, title) {
 	// body...
 }
 
+create("ice cream");
+create("sprinkles");
+create("vanilla");
+create("frosting");
+console.log(getAll());
