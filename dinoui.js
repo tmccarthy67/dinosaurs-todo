a15d728d77d6c38bc0112ce4694a395eb13b1526
ggholson <deviowa@Caseys-MacBook-Air.local>
2014-06-13 11:10:37 -0500

    Added more button functionality

--- dinoui.js
+++ dinoui.js
@@ -1,35 +1,47 @@
-function reload () {
-	console.log("loaded");
-	console.log (getAll());
-	var items = getAll();
-	$("#todoitems").empty();
-	for (i=0; i< items.length; i++) {
-		var item = items[i]
-		var id = item.id;
-		console.log (item.title, item.completed)
-			var checkbox = $("<input>").attr("type","checkbox");
-			var tdCheckbox = $("<td>").append(checkbox).addClass('tododone');
-			var tdTitle = $("<td>").addClass('todotitle').text(item.title);
-			var spanRemove = $("<span>").addClass('glyphicon glyphicon-remove');
-			var buttonRemove = $("<button>").attr("type","button").addClass('btn btn-danger delete').append(spanRemove).attr("todoitemid",id)
+function reload() {
+    console.log("loaded");
+    console.log(getAll());
+    var items = getAll();
+    $("#todoitems").empty();
+    for (i = 0; i < items.length; i++) {
+        var item = items[i]
+        var id = item.id;
+        console.log(item.title, item.completed)
+        var checkbox = $("<input>").attr("type", "checkbox");
+        var tdCheckbox = $("<td>").append(checkbox).addClass('tododone');
+        var tdTitle = $("<td>").addClass('todotitle').text(item.title);
+        var spanRemove = $("<span>").addClass('glyphicon glyphicon-remove');
+        var buttonRemove = $("<button>").attr("type", "button").addClass('btn btn-danger delete').append(spanRemove).attr("todoitemid", id)
 
-			buttonRemove.on("click",function(e) {
-				var todoid = $(this).attr("todoitemid");
-				console.log(remove(todoid));
-				reload();
-			})
+        buttonRemove.on("click", function(e) {
+            var todoid = $(this).attr("todoitemid");
+            console.log(remove(todoid));
+            reload();
+        });
 
+        var tdRemove = $("<td>").addClass('todoremove').append(buttonRemove);
+        var tr = $("<tr>").append(tdCheckbox).append(tdTitle).append(tdRemove);
+        $("#todoitems").append(tr);
 
-
-
-			var tdRemove = $("<td>").addClass('todoremove').append (buttonRemove);
-			var tr =$("<tr>").append(tdCheckbox).append(tdTitle).append(tdRemove);
-			$("#todoitems").append(tr);
-
-			}
+    }
 }
 
 
-$(function (){
-	reload();
+$(function() {
+    $("#inputtext").val("").focus();
+    reload();
+
+    $('#addbutton').on("click", function() {
+
+        var text = $('#inputtext').val();
+        if (text.length === 0) {
+            $('#inputtext').parent().addClass("has-error");
+            return;
+        }
+        $('#inputtext').parent().removeClass("has-error");
+        create(text);
+        reload();
+        $("#inputtext").val("").focus();
+
+    });
 })
